5a92556a5740789d5cc2dd1d4163039f986062a7
chineduCoded <chinedujohn17@gmail.com>
2023-07-13 13:51:08 +0000

    Unique attribute in sql table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Script that creates a table users with the following fields
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
+)

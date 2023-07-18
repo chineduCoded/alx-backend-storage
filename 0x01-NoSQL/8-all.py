519418558ee02627937f00b828d9cf48ea6838b9
chineduCoded <chinedujohn17@gmail.com>
2023-07-18 00:10:53 +0000

    List all document in collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+List all documents in collection
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    function to list all doc in a collection
+    """
+    if mongo_collection is None:
+        return []
+
+    documents = mongo_collection.find()
+    return [post for post in documents]
